377e3964435387d0fde689b1630b59f770356739
Casey Copeland <casey@copeland.tech>
2020-01-09 22:47:39 -0600

    Added application code and unit tests

---
+++ src/Entities/Listify.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ListifyApi.Entities
+{
+    public class Listify : IReadOnlyList<int>
+    {
+        private readonly int _minValue;
+        private readonly int _maxValue;
+
+        public Listify(int minValue, int maxValue)
+        {
+            _minValue = minValue;
+            _maxValue = maxValue;
+
+            Count = maxValue - minValue;
+
+            if (Count <= 0)
+                throw new ArgumentOutOfRangeException($"Class Name: {nameof(Listify)} has a Constructor with the Parameter {nameof(maxValue)} with a value of {maxValue} which is less than the Parameter {nameof(minValue)} with the value of {minValue}");
+        }
+
+        public int this[int index] {
+            get
+            {
+                if (index >= 0 && index <= Count)
+                    return _minValue + index;
+                else
+                    throw new IndexOutOfRangeException($"The index {index} is out of the available range: ({_minValue} - {_maxValue}).");
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public bool Contains(int value)
+        {
+            return value >= _minValue && value <= _maxValue;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (var index = _minValue; index < _maxValue; index++)
+                yield return index;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ test/unit/ListifyControllerTests.cs
@@ -0,0 +1,77 @@
+using ListifyApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ListifyApi.Tests
+{
+    public class ListifyControllerTests
+    {
+        [Fact]
+        public void GetAllReturnsAllValuesNoFilter()
+        {
+            //Given the following setup
+            var controller = new ListifyController();
+            var expectedValues = Enumerable.Range(0, 100); //Default value assigned is 100
+
+            //When the controller is called
+            var values = controller.GetAll(new ListifyFilterModel());
+            var OkResult = values as OkObjectResult;
+
+            //Then the response code should match the expected response code
+            Assert.Equal(200, OkResult.StatusCode);
+            Assert.True(((IEnumerable<int>)OkResult.Value).SequenceEqual(expectedValues));
+        }
+
+        [Theory]
+        [InlineData(0, 100, 200)]
+        [InlineData(100, 100, 400)]
+        [InlineData(-25, 100, 200)]
+        [InlineData(25, 10, 400)]
+        [InlineData(null, 10, 400)]
+        [InlineData(25, null, 400)]
+        [InlineData(null, null, 200)]
+        public void GetAllRequestValidation(int? minValue, int? maxValue, int expectedResponseCode)
+        {
+            //Given the following setup
+            var controller = new ListifyController();
+
+            //When the controller is called
+            var response = controller.GetAll(new ListifyFilterModel() { MinValue = minValue, MaxValue = maxValue });
+            var statusCode = response as IStatusCodeActionResult;
+
+            //Then the response code should match the expected response code
+            Assert.Equal(expectedResponseCode, statusCode.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(1, 0, 100, 200)]
+        [InlineData(10, 10, 100, 200)]
+        [InlineData(9, 10, 100, 200)]
+        [InlineData(-9, 10, 100, 400)]
+        [InlineData(120, 10, 100, 400)]
+        [InlineData(10, null, 100, 400)]
+        [InlineData(10, 10, null, 400)]
+        [InlineData(10, null, null, 200)]
+        [InlineData(0, null, null, 200)]
+        public void GetReturnsValue(int index, int? minValue, int? maxValue, int expectedResponseCode)
+        {
+            //Given the following values
+            var controller = new ListifyController();
+
+            //When the controller is called
+            var response = controller.Get(index, new ListifyFilterModel() { MinValue = minValue, MaxValue = maxValue });
+            var statusCode = response as IStatusCodeActionResult;
+            var responseObject = response as OkObjectResult;
+
+            //Then the response code should match the expected response code
+            Assert.Equal(expectedResponseCode, statusCode.StatusCode);
+
+            if(statusCode.StatusCode == 200)
+                Assert.Equal(minValue.HasValue ? minValue + index : index, (int)responseObject.Value);
+        }
+    }
+}
